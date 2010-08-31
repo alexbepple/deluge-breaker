43f26c2dc291031faf751667989773661ba2b571
Alex Bepple <alex@bepple.de>
2010-08-31 02:53:21 +0200

    Connected to NetworkManager via DBus.

--- src/deluge-breaker.py
+++ src/deluge-breaker.py
@@ -8,6 +8,21 @@ def notify_if_deluge_needs_halting():
     if deluge.is_running() and network.is_dangerous():
         notifier.warn_about_deluge()
 
-if __name__ == "__main__":
-    notify_if_deluge_needs_halting()
+def see_about_deluge_when_network_becomes_available(state):
+    if state == 3:
+        notify_if_deluge_needs_halting()
 
+def set_up_daemon():
+    from dbus.mainloop.glib import DBusGMainLoop
+    import dbus
+    import gobject
+
+    DBusGMainLoop(set_as_default=True)
+    system_bus = dbus.SystemBus()
+
+    system_bus.add_signal_receiver(see_about_deluge_when_network_becomes_available, signal_name="StateChanged", dbus_interface="org.freedesktop.NetworkManager")
+
+    gobject.MainLoop().run()
+
+if __name__ == "__main__":
+    set_up_daemon()
