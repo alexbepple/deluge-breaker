c22cbdcc57a6f28f35d44b99f8e703c652ce55e3
Alex Bepple <alex@bepple.de>
2010-09-02 23:59:07 +0200

    Use reactor.crash() instead of reactor.stop().
    For some reason proper stopping does not exit reactor.run() on the second run.

--- src/delugedriver.py
+++ src/delugedriver.py
@@ -7,24 +7,24 @@ def is_running():
     def is_deluge(cmdline): return "/usr/bin/deluge-gtk" in cmdline
     return filter(is_deluge, processes)
 
+from deluge.log import setupLogger
+setupLogger()
+
 def pause():
     from deluge.ui.client import client
     from twisted.internet import reactor
 
-    from deluge.log import setupLogger
-
-
-    def disconnect(result):
-        client.disconnect()
-        reactor.stop()
-
     def on_connect_success(result):
+
+        def disconnect(result):
+            client.disconnect()
+            reactor.crash()
+
         client.core.pause_all_torrents().addCallback(disconnect)
 
     def on_connect_fail(result):
         print "Connection failed!"
 
-    setupLogger()
     d = client.connect()
     d.addCallback(on_connect_success)
     d.addErrback(on_connect_fail)
