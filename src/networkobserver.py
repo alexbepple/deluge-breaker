31616ea11272934fca844cec7f8d3ec1d7f3a450
Alex Bepple <alex@bepple.de>
2010-09-08 06:09:16 +0200

    Fix: only trigger on connecting.
    NetworkManager is a bitch. The signals StateChanged and PropertiesChanged yield different results with regard to network states.
    
    When manually disabling and reenabling networking, StateChanged yields one more state indicating that it is connected (state 3), when in fact it is not.
    
    StateChanged:
        1, 3, 4, 2, 3
    PropertiesChanged:
        1, 4, 2, 3
    
    I thus switched to PropertiesChanged.

--- src/networkobserver.py
+++ src/networkobserver.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 from dbus.mainloop.glib import DBusGMainLoop
 import dbus
 import gobject
@@ -7,12 +5,16 @@ import gobject
 DBusGMainLoop(set_as_default=True)
 
 def when_network_becomes_available(callback):
-    def on_new_state(state):
-        if state == 3:
+
+    def is_connected(props):
+        return props.get("State") == 3
+
+    def on_new_props(props):
+        if is_connected(props):
             callback()
 
     system_bus = dbus.SystemBus()
-    system_bus.add_signal_receiver(on_new_state, signal_name="StateChanged", dbus_interface="org.freedesktop.NetworkManager")
+    system_bus.add_signal_receiver(on_new_props, signal_name="PropertiesChanged", dbus_interface="org.freedesktop.NetworkManager")
 
 def start():
     gobject.MainLoop().run()
